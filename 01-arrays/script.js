b9d4f0134068668a5dad58bbc44fe89fdf9261e4
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-05-21 15:23:25 -0300

    Exercicio array resolvido

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -2,13 +2,22 @@
 // 1) Adicionar "a" no final das palavras
 const profissionais = ["desenvolvedor", "programador", "autor", "vereador", "professor"];
 
+const profissionaisMelhores = profissionais.map(profissional => {
+  return profissional + "a";
+})
+
 // 2) Retornar frutas com somente a primeira letra maiúscula
 const frutas = ["maçã", "banana", "Pera", "Melancia", "pêsSSEgO", "jAcA"];
 
+const frutasFormatadas = frutas.map(fruta => fruta[0].toUpperCase() + fruta.toLowerCase().substr(1));
+
 
 // filter(): retorna uma nova array com apenas com elementos que atendem à condição
+
 // 1) Criar nova array somente com números pares
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const pares = num.filter(n => n % 2 === 0);
 
 // 2) Criar nova array somente com profissionais da área de TI
 const pessoas = [
@@ -19,10 +28,13 @@ const pessoas = [
   { nome: "Monica", area: "medicina", profissao: "cardiologista"},
 ];
 
+const pessoasTI = pessoas.filter(pessoa => pessoa.area === "TI");
+
 
 // reduce(): recebe uma array e retorna um valor só. Redutor.
-// 1) Dado uma array de números, retornar a soma de todos eles
 
+// 1) Dado uma array de números, retornar a soma de todos eles
+const soma = num.reduce((acc, curr) => acc + curr);
 
 // 2) Retornar o lucro de uma empresa
 const transacoes = [
@@ -32,8 +44,20 @@ const transacoes = [
   { descricao: "salario 1", valor: 8500, tipo: "saída" }
 ];
 
+const lucro = transacoes.reduce((acc, curr) => curr.tipo === "entrada" ? acc + curr.valor : acc - curr.valor, 0)
 
 // sort(): organiza itens da array de acordo com uma verificação
+
 // 1) Organizar números de forma decrescente
+const numDecrescente = [...num].sort((a , b) => b - a);
 
 // 2) Organizar pessoas por ordem alfabética
+const pessoasAlfabetico = [...pessoas].sort((a, b) => {
+  if (a.nome < b.nome) {
+    return -1;
+  } else if (a.nome > b.nome) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
