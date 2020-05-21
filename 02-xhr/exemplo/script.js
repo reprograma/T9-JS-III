fb47c4927e569af9ed561f52724f6a6257c594b6
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-05-21 15:26:17 -0300

    Adicionar exercicio e exemplo de XHR

---
+++ 02-xhr/exemplo/script.js
@@ -0,0 +1,8 @@
+const chamarDemoBtn = document.getElementById('chamarDemo');
+
+const chamarRequisicao = () => {
+  // https://pokeapi.co/api/v2/pokemon/ditto/
+
+}
+
+chamarDemoBtn.addEventListener('click', chamarRequisicao);
