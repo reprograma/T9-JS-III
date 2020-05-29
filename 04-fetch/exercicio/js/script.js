997cb2a56e68ad810efb551bd54b16b4978e7148
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-05-29 14:43:35 -0300

    Mudança de exercício de fetch e adição de exercício do pokemon em fetch

--- 04-fetch/exercicio/js/script.js
+++ 04-fetch/exercicio/js/script.js
@@ -1,4 +1,17 @@
-// Fazer o fetch pra ver se carregamos o array de cartas
 const cardSection = document.getElementById('cards-section')
 const errorSection = document.getElementById('error')
-const loading = document.getElementById('spinner')
+
+const placeholderCard = {
+  nome: "Carta não carregada",
+  tipo: "Arcano Maior",
+  descricao: "Sem descrição",
+  imagem: "http://via.placeholder.com/250X500",
+  link: "https://www.astrolink.com.br/"
+}
+
+renderizaCarta = carta => {
+  const novaCarta = new Card(carta).render()
+  cardSection.innerHTML = novaCarta
+}
+
+renderizaCarta(placeholderCard);
---
+++ 04-fetch/exercicio_pkm/script.js
@@ -0,0 +1,40 @@
+// Criar uma mini aplicação que recebe o nome de um pokemon, acessa sua API e retorna seu nome e imagem
+// A API a ser usada é https://pokeapi.co/api/v2/pokemon/${nome_do_pokemon}/
+// Ver documentação em https://pokeapi.co/
+// A imagem deve ser ter essa URL https://pokeres.bastionbot.org/images/pokemon/${id_do_pokemon}.png
+// Essa id é retirada da API anterior
+
+
+// TRANSFORMAR A REQUISIÇÃO XMLHTTREQUEST EM FETCH
+// 14h30
+
+const form = document.getElementById('pkmForm');
+const input = document.getElementById('pkmInput');
+const pkmPlaceholder = document.getElementById('pkmPlaceholder');
+
+const chamarRequisicao = (e) => {
+  e.preventDefault();
+  pkmPlaceholder.innerHTML = "";
+  const pkmNome = input.value.toLowerCase();
+  const request = new XMLHttpRequest();
+  
+  request.open("GET", `https://pokeapi.co/api/v2/pokemon/${pkmNome}/`, true);
+
+  request.addEventListener("readystatechange", function () {
+    console.log('request', request)
+    if (request.readyState == 4 && request.status == 200) {
+      const data = JSON.parse(request.response);
+      const p = document.createElement('p');
+      const img = document.createElement('img');
+
+      p.textContent = data.species.name;
+      img.setAttribute('src', `https://pokeres.bastionbot.org/images/pokemon/${data.id}.png`)
+
+      pkmPlaceholder.appendChild(p).appendChild(img);
+    }
+  })
+  
+  request.send();
+}
+
+form.addEventListener('submit', (e) => chamarRequisicao(e));
